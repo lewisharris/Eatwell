041d928e56d4dbba3aabd6a189e96382e51b565a
Lewis <lewisdanielharris@gmail.com>
2020-09-27 12:17:08 +0100

    addition of component style

--- client/src/components/misc/ErrorNotice.js
+++ client/src/components/misc/ErrorNotice.js
@@ -1,10 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div``;
+
+//fix bug, component must show nothing when error is cleared
 export default function ErrorNotice(props) {
   return (
-    <div className="error-notice">
+    <Container className="error-notice">
       <span>{props.message}</span>
       <button onClick={props.clearError}>X</button>
-    </div>
+    </Container>
   );
 }
