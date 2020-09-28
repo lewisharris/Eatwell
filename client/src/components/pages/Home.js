2d5fff20f5d46c2991da53fc9cc546e1297316a3
Lewis <lewisdanielharris@gmail.com>
2020-09-28 19:42:28 +0100

    main framework complete

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import UserContext from "../../context/UserContext";
+import { useHistory } from "react-router-dom";
 
 export default function Home() {
+  const { userData } = useContext(UserContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!userData.user) {
+      history.push("/login");
+    }
+  });
   return <div>Home page</div>;
 }
