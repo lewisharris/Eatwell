caf3ff50005c3417b98a735bffca8cdd0b4be65c
Lewis <lewisdanielharris@gmail.com>
2020-09-29 21:34:37 +0100

    working food list

--- client/src/components/layouts/DailyDiary.js
+++ client/src/components/layouts/DailyDiary.js
@@ -3,30 +3,8 @@ import axios from "axios";
 import UserContext from "../../context/UserContext";
 
 export default function DailyDiary() {
-  const [foodData, setFoodData] = useState();
+  const [foodData, setFoodData] = useState([]);
   const { userData } = useContext(UserContext);
-
-  const displayFood = () => {
-    if (foodData === undefined) {
-      return (
-        <tr>
-          <td></td>
-          <td>no entries</td>
-          <td></td>
-        </tr>
-      );
-    } else {
-      foodData.map(item => {
-        return (
-          <tr>
-            <td>{item.title}</td>
-            <td>{item.calories}</td>
-            <td>remove</td>
-          </tr>
-        );
-      });
-    }
-  };
 
   useEffect(() => {
     axios
@@ -36,25 +14,20 @@ export default function DailyDiary() {
       .then(res => {
         setFoodData(res.data);
       })
-      .then(() => {
-        displayFood();
-      })
       .catch(err => {
         console.log(err);
       });
-  }, []);
+  }, [userData]);
 
   return (
     <div>
-      <h3>Breakfast</h3>
-      <table>
-        <thead>
-          <tr>Food</tr>
-          <tr>Calories</tr>
-          <tr>Remove</tr>
-        </thead>
-        <tbody>{displayFood()}</tbody>
-      </table>
+      <ul>
+        {foodData.length === 0 ? (
+          <li key={0}>No Entries</li>
+        ) : (
+          foodData.map(item => <li key={item._id}>{item.title}</li>)
+        )}
+      </ul>
     </div>
   );
 }
