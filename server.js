5752f225810c98eb1bbc7ce46927e4319d337cec
Lewis <lewisdanielharris@gmail.com>
2023-09-14 22:17:39 +0100

    cors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ connection.once("open", () => {
 //middleware
 app.use(
   cors({
-    origin: ["https://eatwell-frontend.vercel.app/"],
+    origin: ["*"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
