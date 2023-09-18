5a7a671bd9558be3c9c4ff38c0134620190d8751
Lewis <lewisdanielharris@gmail.com>
2023-09-18 15:03:06 +0100

    post change

--- server.js
+++ server.js
@@ -42,6 +42,9 @@ app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
+app.post("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   // listen on port for server
